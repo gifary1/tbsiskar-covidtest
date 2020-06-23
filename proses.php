691c2e1a5cd9fd3bd786492c5365dbdef2dbbf46
jayker1 <40833252+jayker1@users.noreply.github.com>
2020-06-23 12:54:23 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,22 @@
+<?php
+include 'koneksi.php';
+include 'subproses.php';
+
+
+if (isset($_POST['submit'])) {
+	$status_id = getUserInput($_POST);
+
+	$id_status = (int) $status_id[0];
+
+	$result = 'SELECT status, info, penanganan FROM tb_status WHERE id = ' . $status_id[0];
+
+	$getStatus = mysqli_fetch_array(mysqli_query($kon, $result));
+
+	$status = $getStatus[0];
+
+	$info = $getStatus[1];
+
+	$penanganan = $getStatus[2];
+
+	include 'hasil.php';
+}
